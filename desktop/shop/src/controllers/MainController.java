efac3bd8e0930eb4bc001d7ba2953229b01f8a56
Sallai András <andrewteki@gmail.com>
2023-02-19 15:44:31 +0100

    desktop: Törlés megerősítéssel működik

--- desktop/shop/src/controllers/MainController.java
+++ desktop/shop/src/controllers/MainController.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import models.Product;
 import models.api.ProductApi;
 import views.MainTab;
@@ -41,14 +44,36 @@ public class MainController {
     }
     private void onActionDelButton() {
         System.out.println("Törlés ...");
-        TableView<Product> productTable = this.mainView.getTablePanel().getTableView();
-        int selected = productTable.getSelectionModel().getSelectedIndex();
-        System.out.println("választott: " + selected);
 
-        Product product = productTable.getItems().get(selected);
+        if (this.confirmDelete()) {
+            TableView<Product> productTable = this.mainView.getTablePanel().getTableView();
+            int selected = productTable.getSelectionModel().getSelectedIndex();
+            if (selected == -1) {
+                this.warningSelection();
+            }else {
+                Product product = productTable.getItems().get(selected);        
+                this.api.delProduct(product.getId());
+                mainView.getTablePanel().getProducts();
+            }
 
-        this.api.delProduct(product.getId());
-        mainView.getTablePanel().getProducts();
+        }
+
+    }
+    private boolean confirmDelete() {
+        Alert alert = new Alert(AlertType.CONFIRMATION );
+        alert.setTitle("Törlés");
+        alert.setHeaderText("Törlés");
+        alert.setContentText("Biztosan törlöd?");
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.OK) 
+            return true;
+        else 
+            return false;
+    }
+    private void warningSelection() {
+        Alert selectionAlert = new Alert(AlertType.WARNING);
+        selectionAlert.setContentText("Nincs kiválasztva sor");
+        selectionAlert.show();
     }
     public MainTab getMainTab() {
         return this.mainTab;
